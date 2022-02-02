cea1863d93a88755354749dc2620a547ad088dba
keyur <keyur@azurework.com>
2022-02-02 14:58:35 -0500

    Initial Demo App

---
+++ AzureB2C_BAPS/BAPS.Sample.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+
+namespace WebApp_OpenIDConnect_DotNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
